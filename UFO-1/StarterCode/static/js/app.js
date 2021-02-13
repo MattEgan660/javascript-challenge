e33396487b3c1b130c099c824889586d92cd7244
MattEgan660 <61856548+MattEgan660@users.noreply.github.com>
2021-02-13 16:06:38 -0500

    added table builder function and have table data displaying correctly on page.

--- UFO-1/StarterCode/static/js/app.js
+++ UFO-1/StarterCode/static/js/app.js
@@ -1,4 +1,43 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+
+
+function tableBuilder(data){
+    tbody.html("");
+    data.forEach((data) => {
+        var row = tbody.append("tr");
+        Object.entries(data).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+};
+
+
+  
+
+
+
+// // sumbitHandler function
+// function submitHandler() {
+//      d3.event.preventDefault();
+     
+//      var inputElement = d3.select("#datetime"); 
+//      var userInput = inputElement.property("value");
+
+
+// };
+
+
+// var button = d3.select("#filter-button")
+// var form = d3.select(".form-control")
+
+// // event handler
+// button.on("click", submitHandler);
+// form.on("submit", submitHandler)
+
+
+tableBuilder(tableData);
